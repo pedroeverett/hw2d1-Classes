add306c600b76efa1e3bbfcb5f7ee1ceb68ab047
Pedro Everett <pedrojoseeverett@gmail.com>
2017-05-29 20:11:25 +0100

    team getters done

--- team/specs/team_specs.rb
+++ team/specs/team_specs.rb
@@ -1,14 +1,20 @@
 require('minitest/autorun')
 require_relative('../team.rb')
 
-class TestGolfTeam
+class Testgolfteam < Minitest::Test
+  def setup
+    @team = Golfteam.new("golfies",["John", "Zed", "Delta"], "Duffy")
+  end
 
-def setup
-  @team = GolfTeam.new[
-    {name: "golfies",
-      players:["John", "Zed", "Delta"], 
-      coach: "Duffy"}
-    ]
-  
-end
+  def test_team_name
+    assert_equal("golfies", @team.team_name)
+  end
+
+  def test_players
+    assert_equal(["John", "Zed", "Delta"], @team.players_names)
+  end
+
+  def test_coach
+    assert_equal("Duffy", @team.coach_name)
+  end
 end
--- team/team.rb
+++ team/team.rb
@@ -1,9 +1,22 @@
-class GolfTeam
+class Golfteam
+  attr_reader :name, :players, :coach
 
-def initialize(name, players, coach)
-  @name = name
-  @players = players
-  @coach = coach
-end
+  def initialize(name, players, coach)
+    @name = name
+    @players = players
+    @coach = coach
+  end
 
+
+  def team_name
+    return @name
+  end
+
+  def players_names
+    return @players
+  end
+
+  def coach_name
+    return @coach
+  end
 end
